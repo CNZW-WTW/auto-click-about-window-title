c6a59e5c97d2d9e55c3d343d0fa4fcd7f697233d
CNZW-WTW <146963503+CNZW-WTW@users.noreply.github.com>
2024-04-05 15:23:58 +0800

    auto.py

--- auto.py
+++ auto.py
@@ -1,8 +1,14 @@
 import pyautogui
 import time
- 
+import pygetwindow as gw
+import os
+
+os.system("cls")
+os.system("title CNZW - https://cnzw-wtw.github.io/")
 print("欢迎使用 CNZW 窗口自动点击工具")
 print("了解详情请访问https://github.com/CNZW-WTW/auto-click-about-window-title")
+print("官网:https://cnzw-wtw.github.io/")
+
 # 设置触发点击的条件，例如当出现特定窗口的标题时
 def on_window_active(title):
     if pyautogui.getActiveWindowTitle() == title:
@@ -15,6 +21,6 @@ def monitor_window(title):
     while True:
         on_window_active(title)
         time.sleep(1)  # 每秒检查一次
- 
+
 # 启动监听窗口标题
 monitor_window('语音通话')
